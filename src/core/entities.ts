04190fad1d528698678544e4e2093938861ac461
Stepan <kvantoreps@gmail.com>
2021-10-07 19:33:08 +0300

    feat(entities): split apartment entity to several ones

--- src/core/entities.ts
+++ src/core/entities.ts
@@ -1,25 +1,29 @@
+interface IAddress {
+  city: string
+  street: string
+  house: string
+  room: string
+}
+
+interface IPerson {
+  type: string
+  id: number
+  attributes: {
+    first_name: string
+    last_name: string
+    middle_name: string
+  }
+}
+
 export interface IApartment {
   type: string
   id: number
   attributes: {
     title: string
     rooms: number
-    address: {
-      city: string
-      street: string
-      house: string
-      room: string
-    },
+    address: IAddress
     area: number
     unit: string
-  },
-  relationships: {
-    type: string
-    id: number
-    attributes: {
-      first_name: string
-      last_name: string
-      middle_name: string
-    }
   }
+  relationships: IPerson
 }
