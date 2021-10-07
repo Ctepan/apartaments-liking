7534873bcf90b2e171590709ca96e4d6c8e97528
Stepan <kvantoreps@gmail.com>
2021-10-07 20:34:42 +0300

    feat(store): add like actions

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,7 +3,7 @@ import { createStore, Store, useStore as baseUseStore } from 'vuex'
 import { IApartment } from '@/core/entities'
 
 interface State {
-  apartments: IApartment[],
+  apartments: IApartment[]
   likedApartments: number[]
 }
 
@@ -15,12 +15,39 @@ export default createStore<State>({
   mutations: {
     setApartments(state, payload: IApartment[]) {
       state.apartments = payload
+    },
+    addLikedApartment(state, payload: number) {
+      state.likedApartments.push(payload)
+    },
+    removeLikedApartment(state, payload: number) {
+      const index = state.likedApartments.indexOf(payload)
+
+      if (index === -1) {
+        return
+      }
+
+      state.likedApartments.splice(index, 1)
     }
   },
   actions: {
-    updateApartments: async function({ commit }) {
+    async updateApartments({ commit }) {
       const apartments = await fetch('/entities.json').then(x => x.json()).then(x => x.response)
       commit('setApartments', apartments)
+    },
+    likeApartment({ commit }, apartmentId: number) {
+      commit('addLikedApartment', apartmentId)
+    },
+    unlikeApartment({ commit }, apartmentId: number) {
+      commit('removeLikedApartment', apartmentId)
+    },
+    toggleApartmentLike({ dispatch, state }, apartmentId: number) {
+      if (state.likedApartments.includes(apartmentId)) {
+        dispatch('unlikeApartment', apartmentId)
+
+        return
+      }
+
+      dispatch('likeApartment', apartmentId)
     }
   }
 })
