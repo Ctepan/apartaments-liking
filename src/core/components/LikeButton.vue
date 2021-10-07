0c91c8c97be87dd48a6c483b148bf3e22baff50a
Степан Мереуца <mereuca@prodoctorov.ru>
2021-10-07 13:05:59 +0300

    feat(core): add like button component

---
+++ src/core/components/LikeButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="apartment-card__like">
+    <img
+      class="apartment-card__like-icon"
+      src="@/assets/icons/heart.svg"
+      alt=""
+    >
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LikeButton'
+})
+</script>
+
+<style scoped lang="scss">
+.like-button {
+  &__like {
+     display: flex;
+     align-items: center;
+     justify-content: center;
+     width: 32px;
+     height: 32px;
+     border-radius: 50%;
+     background: #0005;
+  }
+
+  &__like-icon {
+     height: 20px;
+     width: 20px;
+     color: white;
+     fill: currentColor;
+
+    &_active {
+       fill: currentColor;
+    }
+  }
+}
+</style>
